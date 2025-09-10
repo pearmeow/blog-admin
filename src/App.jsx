f391d58a714661df5093ee4cb2b82b2d320f73f2
Perry Huang <perryhuang2015@gmail.com>
2025-09-10 17:28:12 -0400

    feat: add some api calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./styles/App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [posts, setPosts] = useState(null);
+    useEffect(() => {
+        (async () => {
+            const data = await fetch("http://localhost:3000/posts");
+            if (!data.ok) {
+                return;
+            }
+            const res = await data.json();
+            console.log(res);
+            setPosts(res);
+        })();
+    }, []);
     return (
         <>
-            <p>This is the admin dashboard</p>
+            <p>{posts ? posts[0].text : ""}</p>
         </>
     );
 }
