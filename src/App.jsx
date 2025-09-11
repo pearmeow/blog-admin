705f2d94b9a60cdea0943fa62832a3e757d46cc8
Perry Huang <perryhuang2015@gmail.com>
2025-09-11 15:15:44 -0400

    feat: simplify and move stuff from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import "./styles/App.css";
-import { useState, useEffect } from "react";
+import Login from "./components/Login";
+import Article from "./components/Article";
 
 function App() {
-    const [posts, setPosts] = useState(null);
-    useEffect(() => {
-        (async () => {
-            const data = await fetch("http://localhost:3000/posts");
-            if (!data.ok) {
-                return;
-            }
-            const res = await data.json();
-            console.log(res);
-            setPosts(res);
-        })();
-    }, []);
-    return (
-        <>
-            <p>{posts ? posts[0].text : ""}</p>
-        </>
-    );
+    const jwt = localStorage.getItem("token");
+    return <>{jwt ? <Article /> : <Login />}</>;
 }
 
 export default App;
