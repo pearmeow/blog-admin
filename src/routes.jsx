48d9fd62801eb0bc7093c83599c02030d7ec4e4e
Perry Huang <perryhuang2015@gmail.com>
2025-09-15 14:07:43 -0400

    feat: add routes skeleton

---
+++ src/routes.jsx
@@ -0,0 +1,19 @@
+import Login from "./components/Login";
+
+const routes = [
+    {
+        path: "/",
+        Component: Login,
+    },
+    // {
+    //     path: "/register",
+    // },
+    // {
+    //     path: "/posts",
+    // },
+    // {
+    //     path: "/posts/:id",
+    // },
+];
+
+export default routes;
