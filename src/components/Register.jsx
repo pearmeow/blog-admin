dd83b20ff4dbda5e03111d4c445220dc479efe11
Perry Huang <perryhuang2015@gmail.com>
2026-01-07 19:02:44 -0500

    feat: add register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,68 @@
+import Form from "./Form";
+import Input from "./Input";
+import Button from "./Button";
+import { useState } from "react";
+
 function Register() {
+    const [errorMessage, setErrorMessage] = useState(null);
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const username = formData.get("username");
+        const password = formData.get("password");
+        const confirm = formData.get("confirm");
+        const authorcode = formData.get("authorcode");
+        try {
+            const res = await fetch("http://localhost:3000/authors", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                    confirm,
+                    authorcode,
+                }),
+            });
+            if (res.ok) {
+                let stuff = await res.json();
+                console.log(stuff);
+            } else {
+                setErrorMessage("Username or password is wrong");
+            }
+        } catch (err) {
+            console.log(err);
+            setErrorMessage("Failed to fetch from api");
+            return;
+        }
+    };
     return (
         <>
             <p>Register</p>
+            <p>{errorMessage}</p>
+            <Form onSubmit={handleLogin}>
+                <Input
+                    type="text"
+                    labelName="Username"
+                    name="username"
+                    id={1}
+                />
+                <Input
+                    type="password"
+                    labelName="Password"
+                    name="password"
+                    id={2}
+                />
+                <Input
+                    type="password"
+                    labelName="Confirm Password"
+                    name="confirm"
+                    id={3}
+                />
+                <Input type="text" labelName="Code" name="authorcode" id={4} />
+                <Button type="submit" text="Submit" />
+            </Form>
         </>
     );
 }
