506e2bac89a1b61ed9d743fb1661d03546ab06e9
Perry Huang <perryhuang2015@gmail.com>
2026-02-07 12:08:27 -0500

    feat: use func for auth instead of manual check

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router";
+import isAuthorized from "../auth.jsx";
 function Error() {
     return (
         <>
             <p>This page doesn't exist!</p>
             <p>
-                {localStorage.getItem("token") ? (
+                {isAuthorized() ? (
                     <Link to="/posts">Back to home</Link>
                 ) : (
                     <Link to="/">Back to login</Link>
