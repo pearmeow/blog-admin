e52a2eb64f0eb0cd8e992fbc4ad3e68a4b54f327
Perry Huang <perryhuang2015@gmail.com>
2026-02-07 10:01:46 -0500

    feat: add untested logout component

---
+++ src/components/Logout.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router";
+import Button from "./Button.jsx";
+import Form from "./Form.jsx";
+
+function Logout() {
+    const nav = useNavigate();
+    const handleLogout = () => {
+        localStorage.clear();
+        nav("/");
+    };
+
+    return (
+        <>
+            <Form onSubmit={handleLogout}>
+                <Button type="submit" text="Log out" />
+            </Form>
+        </>
+    );
+}
+
+export default Logout;
